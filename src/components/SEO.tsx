46313f1639c133b4a15d5018b079eed68fe17993
4PPL8 <qasimarshad176@gmail.com>
2025-08-09 12:01:44 +0500

    Add favicon support for Google search results with proper meta tags and configuration files

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -28,6 +28,9 @@ const SEO: React.FC<SEOProps> = ({
         <title>{title}</title>
         <meta name="description" content={description} />
         <link rel="canonical" href={url} />
+        <link rel="icon" type="image/png" href="/SNT2.png" />
+        <link rel="shortcut icon" type="image/png" href="/SNT2.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/SNT2.png" />
         
         {/* Open Graph / Facebook */}
         <meta property="og:type" content={type} />
